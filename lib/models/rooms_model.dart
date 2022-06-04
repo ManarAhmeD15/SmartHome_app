e9c6f20daa09958cbeea62fe4d37c46554de88ab
Ibyaheem <ha585115@gmail.com>
2022-06-04 21:52:39 +0200

    مثاءو

---
+++ lib/models/rooms_model.dart
@@ -0,0 +1,64 @@
+import 'dart:io';
+
+import 'package:image_picker/image_picker.dart';
+class RoomsModel{
+  late String name;
+  late String roomImage;
+  late String uId;
+
+  RoomsModel({
+    required this.roomImage,
+    required this.name,
+    required this.uId,
+  });
+
+
+  RoomsModel.fromJson(Map<String, dynamic> json)
+  {
+    roomImage = json['roomImage'];
+    name = json['name'];
+    uId = json['uId'];
+  }
+
+  Map<String, dynamic> toMap()
+  {
+    return {
+      'name':name,
+      'roomImage':roomImage,
+      'uId':uId,
+    };
+  }
+
+
+}
+
+void add_room({
+  required String name,
+  required String uid,
+
+})
+{
+
+}
+
+
+/*
+ File profileImage;
+
+var picker = ImagePicker();
+
+Future <void> getProfileImage() async
+{
+  final pickedFile = await picker.getImage(
+    source: ImageSource.gallery,
+  );
+  if (pickedFile != null)
+    {
+      profileImage = File(pickedFile.path)
+    } else
+    {
+      print('No image selected');
+    }
+}
+
+*/
