3c1c0ccff134685756e2b9ae6892101ad74000ae
Ibyaheem <ha585115@gmail.com>
2022-08-08 22:55:30 +0200

    Initial Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
-import 'package:beginning_app/modules/all%20rooms/kitchen/kitchen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import 'modules/login/loginscreen.dart';
 import 'modules/navigation/navigation_provider.dart';
 
 void main() async {
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
 
           //title: title,
           //theme: ThemeData(primarySwatch: Colors.deepOrange),
-          home: KitchenScreen(),
+          home: LoginScreen(),
         ),
       );
 }
