b988a874e8e241f11f2fe697addafca08dfa6ff9
Ibyaheem <ha585115@gmail.com>
2022-08-08 23:17:17 +0200

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
+import 'package:beginning_app/modules/all%20rooms/bedroom/bedroom.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-import 'modules/login/loginscreen.dart';
 import 'modules/navigation/navigation_provider.dart';
 
 void main() async {
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
 
           //title: title,
           //theme: ThemeData(primarySwatch: Colors.deepOrange),
-          home: LoginScreen(),
+          home: bedroom(),
         ),
       );
 }
